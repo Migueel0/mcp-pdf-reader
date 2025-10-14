eee407e68d8afe17d718071dfa40b435c8c462c5
Migueel0 <Migueel0@users.noreply.github.com>
2025-10-14 14:05:00 +0000

    refactor: Apply flake8 autopep8 formatting

--- server/server.py
+++ server/server.py
@@ -2,18 +2,20 @@ from mcp.server import FastMCP
 
 server = FastMCP("pdf-reader")
 
+
 @server.tool("read_pdf", description="Reads and extracts text from a PDF file.")
 async def read_pdf(file_path: str) -> str:
     """Reads and extracts text from a PDF file.
 
     Args:
         file_path (str): The path to the PDF file."""
-    
+
     ...
 
 
 def main():
     server.run()
+
 
 if __name__ == "__main__":
     main()
