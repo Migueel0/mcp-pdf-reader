0bedb7066d0af2ed90762a042d77736932b8ab0b
Migueel0 <miggaller@alum.us.es>
2025-12-05 09:16:34 +0100

    feat: Add text extraction from images in pdf files ref: #38

--- tools/pdf_reader.py
+++ tools/pdf_reader.py
@@ -1,4 +1,10 @@
+import io
+from PIL import Image
+import pytesseract
 from pypdf import PdfReader
+
+pytesseract.pytesseract.tesseract_cmd = \
+    r"C:\Program Files\Tesseract-OCR\tesseract.exe"
 
 
 def extract_text_from_pdf(file_path) -> str:
@@ -18,5 +24,14 @@ def extract_text_from_pdf(file_path) -> str:
         page_text = page.extract_text()
         if page_text:
             text_chunks.append(page_text)
+
+        for image_file_object in page.images:
+            try:
+                image = Image.open(io.BytesIO(image_file_object.data))
+                ocr_text = pytesseract.image_to_string(image)
+                if ocr_text:
+                    text_chunks.append(ocr_text)
+            except Exception:
+                continue
 
     return "\n".join(text_chunks)
